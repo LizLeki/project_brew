a1cc347d0cab1b53203f50db305463ecafab01f3
Elizabeth Leki <elizabethleki@gmail.com>
2018-08-26 12:11:02 -0600

    beer themed state brewpub map

--- data_explo.R
+++ data_explo.R
@@ -2,9 +2,16 @@ library(dplyr)
 library(DT)
 library(ggplot2)
 library(viridis)
+library(png)
+library(grid)
 
 state_df<-read.csv("state_level_df.csv", stringsAsFactors = FALSE)
+us_shape<-fiftystater::fifty_states
+beer_mug<-readPNG("beer-vector-art.png") %>%
+  rasterGrob(interpolate = TRUE)
 
+
+##### ----- Creating state summary table -----
 state_summaries<-group_by(state_df, state) %>%
   summarise(state_full = tolower(unique(state_full)),
             brewery_count = n(),
@@ -29,8 +36,6 @@ state_summaries<-group_by(state_df, state) %>%
             regional_per_drinkers = regional_count/drink_pop10k
             )
 
-
-
 state_summaries %>%
   mutate_if(is.numeric, round, digits = 3) %>%
   DT::datatable(rownames = FALSE,
@@ -41,14 +46,16 @@ state_summaries %>%
               )
 )
 
-us_shape<-fiftystater::fifty_states
+
+##### ----- Joining state-level data to geo data -----
 
 us_shape<-left_join(x = us_shape,
                     y = state_summaries,
                     by = c("id" = "state_full"))
-
-
 glimpse(us_shape)
+
+
+##### ----- Summary plots -----
 
 png(filename = "state_brewpubs.png",
     width = 1200,
@@ -57,19 +64,23 @@ png(filename = "state_brewpubs.png",
 ggplot(data = us_shape) +
   geom_polygon(aes(x = long, y = lat, group = group, fill = brewpub_per_drinkers),
                color = "black") +
-  scale_fill_viridis(name = "") +
+  #scale_fill_continuous(name = "", low = "#ffffcc",high = "#ffb300") +
+  scale_fill_viridis(name = "",
+                     option = "inferno",
+                     begin = .65,
+                     direction = -1) +
   coord_equal() +
-  theme_void() +
   labs(title = "United States Brewpubs",
        subtitle = "per 10k Residents age 21+",
        caption = "Source: https://github.com/LizLeki/project_brew") +
+  theme_void() +
   theme(legend.key.size = unit(1, "in"), 
         legend.text = element_text(size = 20),
         plot.title = element_text(size = 40, hjust = .5),
         plot.subtitle = element_text(size = 25, hjust = .5),
         plot.caption = element_text(size = 12)
-  )
-
+  ) +
+  annotation_custom(beer_mug, xmin = -80, xmax = -60, ymax = 35, ymin = 25)
 
 dev.off()
 
